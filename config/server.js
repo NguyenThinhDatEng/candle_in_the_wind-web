3718a85de9d17f3eeb920a3545cd0e8460858ba8
NguyenThinhDatSci <toithienamxinnghe@gmail.com>
2021-11-17 23:54:32 +0700

    constructing database

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c32238234d5d6724364ae513fa0de435'),
+    },
+  },
+});
