eb5848dd9791dcbb18a914cb076103689dc172cc
NguyenThinhDatSci <toithienamxinnghe@gmail.com>
2021-11-24 18:46:04 +0700

    add utils, admin, a components

--- back-end/api/customer/controllers/customer.js
+++ back-end/api/customer/controllers/customer.js
@@ -1,8 +1,19 @@
 'use strict';
 
+const Response = require(`../../../utils/response`);
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async login(context){
+        const { email , pw } = context.request.body;
+        const rs = await strapi.query(`customer`).findOne({ email, pw});
+        if(rs){
+            return Response.ok(context, { data: rs, msg: `OK`, status: 0 });
+        }
+        return Response.badRequest(context, {data: null, msg: `Not Found`, status: 0 })
+    }
+};
