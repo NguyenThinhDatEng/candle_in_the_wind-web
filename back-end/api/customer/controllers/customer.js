e8d527533513cf2f4319fb9a0af9d2eb972dc3fd
NguyenThinhDatSci <toithienamxinnghe@gmail.com>
2021-12-12 07:41:02 +0700

    add resetPassword function

--- back-end/api/customer/controllers/customer.js
+++ back-end/api/customer/controllers/customer.js
@@ -1,23 +1,38 @@
 "use strict";
-
+require('dotenv').config();
 const Response = require(`../../../utils/response`);
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
-
 module.exports = {
+  resetPassWord: async (ctx) => {
+    const { email } = ctx.request.body;
+    const emailCheck = await strapi.query('customer').findOne({email})
+    if (emailCheck)
+    {
+      strapi.services.email.send(process.env.user, email, 'Password Reset', 'Your code is: 123abc')
+      return Response.ok(ctx, {
+        status : 200,
+        msg: `${email} is existed, allow change of password`
+      })
+    }
+    else {
+      return Response.badRequest(ctx, {
+        status : 400,
+        msg : 'Not Found'
+      })
+    }
+  },
+
   signup: async (ctx) => {
     const { username, email, password, gender, dateOfBirth, phoneNumber } = ctx.request.body;
     const emailCheck = await strapi.query(`customer`).findOne({email})
     if (emailCheck)
         return Response.notAcceptable(ctx, {
             msg: `${email} already exists`,
+            status : 406
         })
     const rs = await strapi.query(`customer`).create({username, email, password, gender, dateOfBirth, phoneNumber});
     if (rs) {
-      return Response.created(ctx, { data: rs, msg: `Successful`, status: 0 });
+      return Response.created(ctx, { data: rs, msg: `Successful`, status: 201 });
     }
   },
 
@@ -25,12 +40,12 @@ module.exports = {
     const { email, password } = ctx.request.body;
     const rs = await strapi.query(`customer`).findOne({ email, password });
     if (rs) {
-      return Response.ok(ctx, { data: rs, msg: `OK`, status: 0 });
+      return Response.ok(ctx, { data: rs, msg: `OK`, status: 200 });
     }
     return Response.badRequest(ctx, {
       data: null,
       msg: `Not Found`,
-      status: 0,
+      status: 400,
     });
   },
 };
