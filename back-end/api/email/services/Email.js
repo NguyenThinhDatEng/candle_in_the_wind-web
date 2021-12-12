0f6c47e810e8eadb1ba9d35a30a26e78fa72a4a2
NguyenThinhDatSci <toithienamxinnghe@gmail.com>
2021-12-12 07:21:23 +0700

    add mail services

---
+++ back-end/api/email/services/Email.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const nodemailer = require("nodemailer");
+const Response = require(`../../../utils/response`);
+
+// Create reusable transporter object using SMTP transport.
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.user,
+    pass: process.env.pass
+  },
+});
+
+module.exports = {
+  send: (from, to, subject, text) => {
+    // Setup e-mail data.
+    const options = {
+      from,
+      to,
+      subject,
+      text,
+    };
+
+    // Return a promise of the function that sends the email.
+    return transporter.sendMail(options, function(err, data) {
+        if(err) {
+            console.log('Error')
+        } else {
+            console.log('Email sent successfully');
+        }
+    });
+  },
+};
