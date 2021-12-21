f5ed7ffdf5be9db53a3e72165a26194d0cd9f916
NguyenThinhDatSci <toithienamxinnghe@gmail.com>
2021-12-21 09:35:12 +0700

    updated

--- front-end/src/test.mjs
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const id = "6198a1bf1f77dd39e0429988"
-const {data}  = await axios.get(
-    'https://admin-workspace.azurewebsites.net/posts/' 
-);
-
-console.log(data);
