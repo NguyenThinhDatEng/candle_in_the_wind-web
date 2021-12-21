36117cfbd935ad6c8f89bf1c8fc2bc8c993df517
dinhphu2k1-gif <dinhphu012001@gamil.com>
2021-12-21 09:50:42 +0700

    add

---
+++ front-end/src/components/login-signup/signup.js
@@ -0,0 +1,74 @@
+import React from 'react'
+
+export default function Signup() {
+    return (
+        <div>
+            <div className="container-fluid bg-login login-container">
+                <main className="signup-form">
+                    <form>
+                        <h1 className="login-title mb-4">Sign up</h1>
+                        <div className="form-group row mb-3">
+                            <label htmlFor="username" className="col-sm-2 col-form-label">Username</label>
+                            <div className="col-sm-10">
+                                <input type="text" className="form-control" id="username" />
+                            </div>
+                        </div>
+                        <div className="form-group row mb-3">
+                            <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
+                            <div className="col-sm-10">
+                                <input type="email" className="form-control" id="email" />
+                            </div>
+                        </div>
+                        <div className="form-group row mb-3">
+                            <label htmlFor="password" className="col-sm-2 col-form-label">Password</label>
+                            <div className="col-sm-10">
+                                <input type="password" className="form-control" id="password" />
+                            </div>
+                        </div>
+                        <div className="form-group row mb-3">
+                            <label htmlFor="verify-password" className="col-sm-2 col-form-label">Password</label>
+                            <div className="col-sm-10">
+                                <input type="password" className="form-control" id="verify-password" />
+                            </div>
+                        </div>
+                        <fieldset className="form-group row mb-3">
+                            <legend className="col-form-label col-sm-2 pt-0">Gender</legend>
+                            <div className="col-sm-10 ">
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="gridRadios" id="male" defaultValue="option1" defaultChecked />
+                                    <label className="form-check-label" htmlFor="male">
+                                        Male
+                                    </label>
+                                </div>
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="gridRadios" id="female" defaultValue="option2" />
+                                    <label className="form-check-label" htmlFor="female">
+                                        Female
+                                    </label>
+                                </div>
+                            </div>
+                        </fieldset>
+                        <div className="form-group row mb-3">
+                            <label htmlFor="date-of-birth" className="col-sm-2 col-form-label">Date of Birth</label>
+                            <div className="col-sm-10">
+                                <input type="date" className="form-control text-center" id="date-of-birth" />
+                            </div>
+                        </div>
+                        <div className="form-group row mb-3">
+                            <label htmlFor="phone-number" className="col-sm-2 col-form-label">Phone number</label>
+                            <div className="col-sm-10">
+                                <input type="tel" className="form-control" id="phone-number" />
+                            </div>
+                        </div>
+                        <div className="form-group row mb-3 text-center">
+                            <div className="col-sm-12">
+                                <button type="submit" className="btn btn-primary ">Done</button>
+                            </div>
+                        </div>
+                    </form>
+                </main>
+            </div>
+
+        </div>
+    )
+}
---
+++ front-end/src/components/store/store.decoration.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+require('dotenv').config();
+
+export default function StoreDecoration(props) {
+    const data = props.data
+
+    return (
+        data.map(value => {
+            console.log(value)
+            if (value.catalog.name == "decoration") {
+                return (
+                    <div className="col">
+                        <div className="item text-center">
+                            <Link to={`/products/${value._id}`}>
+                                <div className="item-img">
+                                    <img alt="" src={process.env.REACT_APP_DB_URL + value?.image[0]?.url} />
+                                </div>
+                            </Link>
+
+
+                            <div>
+                                <Link to={`/products/${value._id}`}>
+                                    <p className="mt-3"> {value.name} </p>
+                                </Link>
+                                <p className="text-danger"> {value.price}  VNĐ</p>
+                                <button className="btn btn-dark mb-3">Add to cart</button>
+                            </div>
+
+                        </div>
+                    </div>
+                )
+            }
+
+        })
+    )
+}
