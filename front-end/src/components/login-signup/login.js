660ce45d4d86c1bd490ad7fd2b5b7e1e79d0f875
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2021-12-28 20:28:56 +0700

    update login.js

--- front-end/src/components/login-signup/login.js
+++ front-end/src/components/login-signup/login.js
@@ -1,30 +1,77 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./login.css";
 
-export default function Login() {
-    return (
-        <div>
-            <div className="container-fluid bg-login login-container">
-                <main className="login-form">
-                    <form>
-                        <h1 className="login-title mb-4 ">Sign in</h1>
-                        <div className="form-group">
-                            <label htmlFor="email">Email address</label>
-                            <input type="email" className="form-control mb-4" id="email" placeholder="Enter email" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password" className="label">Password</label>
-                            <input type="password" className="form-control mb-4" id="password" placeholder="Password" />
-                        </div>
-                        <div className="text-center">
-                            <p className="forgot-password"> <a href="#">Forgot password?</a> </p>
-                            <div className="clearfix" />
-                            <button type="submit" className="btn btn-success my-3">Sign in</button>
-                            <p>Don't have an account? <Link to="/signup">Sign up</Link> </p>
-                        </div>
-                    </form>
-                </main>
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isShowPassword, setIsShowPassword] = useState(false);
+
+  const handleShowHidePassword = () => {
+    setIsShowPassword(!isShowPassword);
+  };
+  return (
+    <div>
+      <div className="container-fluid bg-login login-container">
+        <main className="login-form">
+          <form>
+            <h1 className="login-title mb-4 ">Sign in</h1>
+            <div className="form-group">
+              <label htmlFor="email">Email address</label>
+              <input
+                type="email"
+                className="form-control mb-4"
+                name="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  //   console.log(email);
+                }}
+              />
             </div>
-        </div>
-    )
-}
+            <div className="form-group">
+              <label htmlFor="password" className="label">
+                Password
+              </label>
+              <div class="customize-input-password">
+                <input
+                  type={isShowPassword ? "text" : "password"}
+                  className="form-control mb-4"
+                  id="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    //   console.log(password);
+                  }}
+                />
+                <span onClick={handleShowHidePassword}>
+                  <i
+                    class={isShowPassword ? "fas fa-eye" : "fas fa-eye-slash"}
+                  ></i>
+                </span>
+              </div>
+            </div>
+            <div className="text-center">
+              <p className="forgot-password">
+                {" "}
+                <a href="#">Forgot password?</a>{" "}
+              </p>
+              <div className="clearfix" />
+              <button type="submit" className="btn btn-success my-3">
+                Sign in
+              </button>
+              <p>
+                Don't have an account? <Link to="/signup">Sign up</Link>{" "}
+              </p>
+            </div>
+          </form>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
