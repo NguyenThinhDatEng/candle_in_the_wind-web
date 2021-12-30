892cfabf95e50a569d8216802b9479dd7fdf780d
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2021-12-30 11:04:56 +0700

    update login api

--- back-end/api/customer/controllers/customer.js
+++ back-end/api/customer/controllers/customer.js
@@ -1,51 +1,64 @@
 "use strict";
-require('dotenv').config();
+require("dotenv").config();
 const Response = require(`../../../utils/response`);
 
 module.exports = {
   resetPassWord: async (ctx) => {
     const { email } = ctx.request.body;
-    const emailCheck = await strapi.query('customer').findOne({email})
-    if (emailCheck)
-    {
-      strapi.services.email.send(process.env.user, email, 'Password Reset', 'Your code is: 123abc')
+    const emailCheck = await strapi.query("customer").findOne({ email });
+    if (emailCheck) {
+      strapi.services.email.send(
+        process.env.user,
+        email,
+        "Password Reset",
+        "Your code is: 123abc"
+      );
       return Response.ok(ctx, {
-        status : 200,
-        msg: `FOUND`
-      })
-    }
-    else {
+        status: 200,
+        msg: `FOUND`,
+      });
+    } else {
       return Response.badRequest(ctx, {
-        status : 400,
-        msg : 'Not Found'
-      })
+        status: 400,
+        msg: "Not Found",
+      });
     }
   },
 
   signup: async (ctx) => {
-    const { username, email, password, gender, dateOfBirth, phoneNumber } = ctx.request.body;
-    const emailCheck = await strapi.query(`customer`).findOne({email})
+    const { username, email, password, gender, dateOfBirth, phoneNumber } =
+      ctx.request.body;
+    const emailCheck = await strapi.query(`customer`).findOne({ email });
     if (emailCheck)
-        return Response.notAcceptable(ctx, {
-            msg: `${email} already exists`,
-            status : 406
-        })
-    const rs = await strapi.query(`customer`).create({username, email, password, gender, dateOfBirth, phoneNumber});
+      return Response.notAcceptable(ctx, {
+        msg: `${email} already exists`,
+        status: 406,
+      });
+    const rs = await strapi
+      .query(`customer`)
+      .create({ username, email, password, gender, dateOfBirth, phoneNumber });
     if (rs) {
-      return Response.created(ctx, { data: rs, msg: `Successful`, status: 201 });
+      return Response.created(ctx, {
+        data: rs,
+        msg: `Successful`,
+        status: 201,
+      });
     }
   },
 
   login: async (ctx) => {
     const { email, password } = ctx.request.body;
-    const rs = await strapi.query(`customer`).findOne({ email, password });
-    if (rs) {
-      return Response.ok(ctx, { data: rs, msg: `OK`, status: 200 });
+    console.log(`email: ${email}, password: ${password}`);
+    const user = await strapi
+      .query(`customer`)
+      .findOne({ email, password }, ["customer.email"]);
+    if (user) {
+      return Response.ok(ctx, { data: user.email, msg: `OK`, status: 1 });
     }
-    return Response.badRequest(ctx, {
+    return Response.ok(ctx, {
       data: null,
-      msg: `Not Found`,
-      status: 400,
+      msg: `User account or password incorrect`,
+      status: 0,
     });
   },
 };
