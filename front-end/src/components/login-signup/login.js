436ea6672ab59b30a824ed10e368201c03b68b92
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2022-01-03 17:00:15 +0700

    updated login.js

--- front-end/src/components/login-signup/login.js
+++ front-end/src/components/login-signup/login.js
@@ -34,12 +34,11 @@ const Login = () => {
       setErrPopUp("Please enter a part following '@'");
     else {
       try {
-        console.log("before", dismissPopup);
         setErrPopUp("checking...");
         await handleCheckEmail(emailReset).then((response) => {
           setDismissPopUp(true);
           setErrPopUp("");
-          console.log("after  ", dismissPopup);
+          setEmailReset("");
         });
       } catch (error) {
         console.log("login.js", error);
@@ -132,55 +131,65 @@ const Login = () => {
                   Forgot password?
                 </a>{" "}
               </p>
-              <form>
-                {/* The Modal */}
-                <div className="modal" id="myModal">
-                  <div className="modal-dialog modal-dialog modal-dialog-centered">
-                    <div className="modal-content">
-                      {/* Modal Header */}
-                      <div className="modal-header">
-                        <h4 className="modal-title">Forgot Password?</h4>
-                        <button
-                          type="button"
-                          className="btn-close"
-                          data-bs-dismiss="modal"
-                          onClick={() => setErrPopUp("")}
-                        ></button>
+              {/* The Modal */}
+              <div className="modal" id="myModal">
+                <div className="modal-dialog modal-dialog modal-dialog-centered">
+                  <div className="modal-content">
+                    {/* Modal Header */}
+                    <div className="modal-header">
+                      <h4 className="modal-title">
+                        {dismissPopup
+                          ? "Code Verification"
+                          : "Forgot Password?"}
+                      </h4>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        onClick={() => setErrPopUp("")}
+                      ></button>
+                    </div>
+
+                    {/* Modal body */}
+                    <div className="modal-body">
+                      <div className="resetTitle">
+                        <b>
+                          {dismissPopup
+                            ? "We've sent a password reset otp to your email"
+                            : "You can reset your password here"}
+                        </b>
                       </div>
-                      {/* Modal body */}
-                      <div className="modal-body">
-                        <div className="resetTitle">
-                          You can reset your password here.
-                        </div>
-                        <input
-                          type="email"
-                          name="email"
-                          placeholder="your email address"
-                          className="form-control mb-2"
-                          onChange={(e) => {
-                            setEmailReset(e.target.value);
-                            setErrPopUp("");
-                          }}
-                        />
+                      <input
+                        type="email"
+                        name="email"
+                        value={emailReset}
+                        placeholder={
+                          dismissPopup ? "Enter code" : "your email address"
+                        }
+                        className="form-control mb-2"
+                        onChange={(e) => {
+                          setEmailReset(e.target.value);
+                          setErrPopUp("");
+                        }}
+                      />
+                    </div>
+                    {/* Modal footer */}
+                    <div className="my-modal-footer">
+                      <div className="errPopUp" style={{ color: "red" }}>
+                        {errPopUp}
                       </div>
-                      {/* Modal footer */}
-                      <div className="my-modal-footer">
-                        <div className="errPopUp" style={{ color: "red" }}>
-                          {errPopUp}
-                        </div>
-                        <button
-                          type="button"
-                          className="btn btn-outline-light"
-                          data-bs-dismiss={dismissPopup ? "modal" : ""}
-                          onClick={handleOnClickReset}
-                        >
-                          Reset
-                        </button>
-                      </div>
+                      <button
+                        type="button"
+                        className="btn btn-outline-light"
+                        data-bs-dismiss={dismissPopup ? "modal" : ""}
+                        onClick={handleOnClickReset}
+                      >
+                        Reset
+                      </button>
                     </div>
                   </div>
                 </div>
-              </form>
+              </div>
               {/* Sign in */}
               <div className="clearfix" />
               <div style={{ color: "red" }}>{errMessage}</div>
