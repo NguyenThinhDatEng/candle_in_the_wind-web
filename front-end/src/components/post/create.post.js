b8ea97a3ef6af6e9d52aaea7646f28dc714563a8
dinhphu2k1-gif <dinhphu012001@gmail.com>
2022-01-05 16:05:19 +0700

    complete comment

--- front-end/src/components/post/create.post.js
+++ front-end/src/components/post/create.post.js
@@ -3,44 +3,81 @@ import Header from "../header/header"
 import Footer from '../footer/footer'
 import { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
-import { handleSignInAPI } from "../../services/customerService";
+import { handleCreatePost } from "../../services/customerService";
 import axios from 'axios';
 require("dotenv").config();
 
 
 export default function CreatePost() {
-    // const [state, setState] = useState({
-    // 	title: "",
-    //     content:""
-    // });
+    const [state, setState] = useState({
+        title: "",
+        content: "",
+        image: null
+    })
+    const [formErrors, setFormErrors] = useState("");
 
-    const [title, setTitle] = useState("")
-    const [content, setContent] = useState("")
-    const [image, setImage] = useState(null)
+    const customer_id = JSON.parse(localStorage.getItem("user-info")).id
 
-    // var customer_id
-    // useEffect(() => {
-    // 	if (localStorage.getItem("user-info")) {
-    // 		customer_id = localStorage.getItem("user-info")
-    // 	}
-    // });
+    const updateTitle = (e) => {
+        setState(previousState => {
+            return { ...previousState, title: e.target.value }
+        });
+    }
 
-    console.log(image)
+    const updateContent = (e) => {
+        setState(previousState => {
+            return { ...previousState, content: e.target.value }
+        });
+    }
 
-    const onFileUpload = () => {
+    const updateImage = (e) => {
+        setState(previousState => {
+            return { ...previousState, image: e.target.files[0] }
+        });
+    }
 
-        // Create an object of formData
-        const formData = new FormData();
+    const handleCreate = async () => {
+        // console.log(state.title, state.content, state.image)
+        if (!state.title) {
+            setFormErrors("Title cannot be empty ")
+            console.log(formErrors)
+        }
+        else if (!state.content) {
+            setFormErrors("Content cannot be empty")
+            console.log(formErrors)
+        }
+        else {
+            setFormErrors("")
+            const formData = new FormData();
+            // Update the formData object
+            formData.append(
+                "files",
+                state.image
+            );
 
-        // Update the formData object
-        formData.append(
-            "image",
-            image,
-            image.name
-        );
-        axios.post(process.env.REACT_APP_SERVER_URL +  "/api/uploadfile", formData);
-    };
+            // Details of the uploaded file
+            let res;
+            await axios.post(process.env.REACT_APP_SERVER_URL + "/upload", formData)
+                .then((response) => {
+                    console.log(response);
+                    res = response
+                })
+            
+            console.log(res?.data[0]?._id)
+            let data = {
+                title: state.title,
+                content: state.content,
+                image : res?.data[0]?.url,
+                customer_id: customer_id
+            }
 
+            // console.log(data)
+            // await handleCreatePost(data).then((response) => {
+            //     console.log(response);
+            // })
+        }
+
+    }
 
     return (
         <div>
@@ -55,18 +92,19 @@ export default function CreatePost() {
                         <form>
                             <div className="form-group mb-4">
                                 <label htmlFor="title">Title</label>
-                                <input type="text" className="form-control" name="title" onChange={(e) => setTitle(e.target.value)} />
+                                <input type="text" className="form-control" name="title" onChange={updateTitle} />
                             </div>
                             <div className="form-group mb-4">
                                 <label htmlFor="description">Description</label>
-                                <textarea rows={5} className="form-control" name="description" defaultValue={""} onChange={(e) => setContent(e.target.value)} />
+                                <textarea rows={5} className="form-control" name="description" defaultValue={""} onChange={updateContent} />
                             </div>
                             <div className="form-group mb-4">
                                 <label htmlFor="formFile" className="form-label">Upload image</label>
-                                <input className="form-control" type="file" id="formFile" onChange={(e) => setImage(e.target.files[0])} />
+                                <input className="form-control" type="file" id="formFile" onChange={updateImage} />
                             </div>
                             <div className="form-group my-5 text-center">
-                                <Link to="/blog" type="submit" className="btn btn-dark" onClick={() => onFileUpload()}>
+                                <p className='text-danger justify-content-center' > {formErrors} </p>
+                                <Link to="#!" type="submit" className="btn btn-dark" onClick={() => handleCreate()} >
                                     Create post
                                 </Link>
                             </div>
