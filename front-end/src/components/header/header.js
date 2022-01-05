fed1578ee7fddeb02aba0d1730b358656cd3eb7b
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2022-01-05 20:44:50 +0700

    Le Tai - change header when sign in successfully

--- front-end/src/components/header/header.js
+++ front-end/src/components/header/header.js
@@ -1,51 +1,130 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import './header.css'
-import { CartContext } from '../../context/Context'
+import React, { useContext, useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./header.css";
+import { CartContext } from "../../context/Context";
 
 export default function Header() {
-    const {cart} = useContext(CartContext)
-    const [total, setTotal] = useState()
-    useEffect(() => {
-        setTotal(cart.reduce((acc, curr) => acc+ Number(curr?.quantity) , 0))
-
-    }, [cart])
-
+  const { cart } = useContext(CartContext);
+  const [total, setTotal] = useState();
+  useEffect(() => {
+    setTotal(cart.reduce((acc, curr) => acc + Number(curr?.quantity), 0));
+  }, [cart]);
+  if (localStorage.getItem("user-info")) {
     return (
-        <header>
-
-            <div className="search">
-                <div className="search-container">
-                    <input type="text" id="search-bar" placeholder="Search product..." />
-                    <a href="#"><img className="search-icon" src="/assets/icons/Search-icon.png" /></a>
-                </div>
+      <header>
+        <div className="search">
+          <div className="search-container">
+            <input
+              type="text"
+              id="search-bar"
+              placeholder="Search product..."
+            />
+            <a href="#">
+              <img
+                className="search-icon"
+                src="/assets/icons/Search-icon.png"
+              />
+            </a>
+          </div>
+        </div>
+        <nav className="navigation">
+          <div className="logo">
+            <img src="/assets/images/Logo.png" alt="Logo" />
+          </div>
+          <ul className="link_webpages">
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to="/store">STORE</Link>
+            </li>
+            <li>
+              <Link to="/blog">BLOG</Link>
+            </li>
+            <li>
+              <Link to="/about-us">ABOUT US</Link>
+            </li>
+          </ul>
+          <div className="icon_nav">
+            <div className="user">
+              <Link to="/profile">
+                <img src="/assets/icons/User-icon.png" />
+              </Link>
             </div>
-            <nav className="navigation">
-                <div className="logo">
-                    <img src="/assets/images/Logo.png" alt="Logo" />
-                </div>
-                <ul className="link_webpages">
-                    <li><Link to="/">HOME</Link></li>
-                    <li><Link to="/store" >STORE</Link></li>
-                    <li><Link to="/blog" >BLOG</Link></li>
-                    <li><Link to="/about-us" >ABOUT US</Link></li>
-                </ul>
-                <div className="icon_nav">
-                    <div className="user">
+            <div className="cart">
+              <Link to="/cart">
+                <img src="/assets/icons/ShoppingCart.png" />
+              </Link>
+              <span class="badge badge-warning" id="lblCartCount">
+                {total}
+              </span>
+            </div>
+          </div>
+          {/* <div className='sign'>
+                        <div className='in'><Link to="/login" >Sign in</Link></div>
+                        <div className='up'><Link to="/signup" >Sign up</Link></div>
+                    </div> */}
+        </nav>
+      </header>
+    );
+  } else
+    return (
+      <header>
+        <div className="search">
+          <div className="search-container">
+            <input
+              type="text"
+              id="search-bar"
+              placeholder="Search product..."
+            />
+            <a href="#">
+              <img
+                className="search-icon"
+                src="/assets/icons/Search-icon.png"
+              />
+            </a>
+          </div>
+        </div>
+        <nav className="navigation">
+          <div className="logo">
+            <img src="/assets/images/Logo.png" alt="Logo" />
+          </div>
+          <ul className="link_webpages">
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to="/store">STORE</Link>
+            </li>
+            <li>
+              <Link to="/blog">BLOG</Link>
+            </li>
+            <li>
+              <Link to="/about-us">ABOUT US</Link>
+            </li>
+          </ul>
+          <div className="icon_nav">
+            {/* <div className="user">
                         <Link to="/profile" ><img src="/assets/icons/User-icon.png" /></Link>
-                    </div>
-                    <div className="cart">
-                        <Link to="/cart" ><img src="/assets/icons/ShoppingCart.png" /></Link>
-                        <span className='badge badge-warning' id='lblCartCount'>{total}</span>
-                    </div>
-                </div>
-                {/* <div className='sign'>
-                    <div className='in'><Link to="/login" >Sign in</Link></div>
-                    <div className='up'><Link to="/signup" >Sign up</Link></div>
-                </div> */}
-            </nav>
-        </header>
-
-
-    )
+                    </div> */}
+            <div className="cart">
+              <Link to="/cart">
+                <img src="/assets/icons/ShoppingCart.png" />
+              </Link>
+              <span class="badge badge-warning" id="lblCartCount">
+                {total}
+              </span>
+            </div>
+          </div>
+          <div className="sign">
+            <div className="in">
+              <Link to="/login">Sign in</Link>
+            </div>
+            <div className="up">
+              <Link to="/signup">Sign up</Link>
+            </div>
+          </div>
+        </nav>
+      </header>
+    );
 }
