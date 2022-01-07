118caae25874bd693a45b557e5171f36a73a458b
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2022-01-08 00:41:19 +0700

    add postService.js

--- front-end/src/components/post/create.post.js
+++ front-end/src/components/post/create.post.js
@@ -1,118 +1,138 @@
-import React from 'react'
-import Header from "../header/header"
-import Footer from '../footer/footer'
+import React from "react";
+import Header from "../header/header";
+import Footer from "../footer/footer";
 import { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
-import { handleCreatePost } from "../../services/customerService";
-import axios from 'axios';
+import { handleCreatePost } from "../../services/postService";
+import axios from "axios";
 require("dotenv").config();
 
-
 export default function CreatePost() {
-    const [state, setState] = useState({
-        title: "",
-        content: "",
-        image: null
-    })
-    const [formErrors, setFormErrors] = useState("");
+  const [state, setState] = useState({
+    title: "",
+    content: "",
+    image: null,
+  });
+  const [formErrors, setFormErrors] = useState("");
 
-    const customer_id = JSON.parse(localStorage.getItem("user-info"))?.id
+  const customer_id = JSON.parse(localStorage.getItem("user-info"))?.id;
 
-    const updateTitle = (e) => {
-        setState(previousState => {
-            return { ...previousState, title: e.target.value }
+  const updateTitle = (e) => {
+    setState((previousState) => {
+      return { ...previousState, title: e.target.value };
+    });
+  };
+
+  const updateContent = (e) => {
+    setState((previousState) => {
+      return { ...previousState, content: e.target.value };
+    });
+  };
+
+  const updateImage = (e) => {
+    setState((previousState) => {
+      return { ...previousState, image: e.target.files[0] };
+    });
+  };
+
+  const handleCreate = async () => {
+    // console.log(state.title, state.content, state.image)
+    if (!state.title) {
+      setFormErrors("Title cannot be empty ");
+      console.log(formErrors);
+    } else if (!state.content) {
+      setFormErrors("Content cannot be empty");
+      console.log(formErrors);
+    } else {
+      setFormErrors("");
+      const formData = new FormData();
+      // Update the formData object
+      formData.append("files", state.image);
+
+      // Details of the uploaded file
+      let res;
+      await axios
+        .post(process.env.REACT_APP_SERVER_URL + "/upload", formData)
+        .then((response) => {
+          console.log(response);
+          res = response;
         });
+
+      console.log(res?.data[0]?._id);
+      let data = {
+        title: state.title,
+        content: state.content,
+        image_id: res?.data[0]?._id,
+        customer_id: customer_id,
+      };
+
+      console.log(data);
+      await handleCreatePost(data).then((response) => {
+        console.log(response);
+      });
     }
+  };
 
-    const updateContent = (e) => {
-        setState(previousState => {
-            return { ...previousState, content: e.target.value }
-        });
-    }
-
-    const updateImage = (e) => {
-        setState(previousState => {
-            return { ...previousState, image: e.target.files[0] }
-        });
-    }
-
-    const handleCreate = async () => {
-        // console.log(state.title, state.content, state.image)
-        if (!state.title) {
-            setFormErrors("Title cannot be empty ")
-            console.log(formErrors)
-        }
-        else if (!state.content) {
-            setFormErrors("Content cannot be empty")
-            console.log(formErrors)
-        }
-        else {
-            setFormErrors("")
-            const formData = new FormData();
-            // Update the formData object
-            formData.append(
-                "files",
-                state.image
-            );
-
-            // Details of the uploaded file
-            let res;
-            await axios.post(process.env.REACT_APP_SERVER_URL + "/upload", formData)
-                .then((response) => {
-                    console.log(response);
-                    res = response
-                })
-            
-            console.log(res?.data[0]?._id)
-            let data = {
-                title: state.title,
-                content: state.content,
-                image_id : res?.data[0]?._id,
-                customer_id: customer_id
-            }
-
-            console.log(data)
-            await handleCreatePost(data).then((response) => {
-                console.log(response);
-            })
-        }
-
-    }
-
-    return (
-        <div>
-            <Header />
-            <div className="container page-title">
-                <p className="text-center py-4">BLOG</p>
-            </div>
-            <div className="container bg-secondary">
-                <div className="row justify-content-center">
-                    <div className="col-md-8 col-md-offset-2">
-                        <h1 className="text-center my-4">Create a new post</h1>
-                        <form>
-                            <div className="form-group mb-4">
-                                <label htmlFor="title">Title</label>
-                                <input type="text" className="form-control" name="title" onChange={updateTitle} />
-                            </div>
-                            <div className="form-group mb-4">
-                                <label htmlFor="description">Description</label>
-                                <textarea rows={5} className="form-control" name="description" defaultValue={""} onChange={updateContent} />
-                            </div>
-                            <div className="form-group mb-4">
-                                <label htmlFor="formFile" className="form-label">Upload image</label>
-                                <input className="form-control" type="file" id="formFile" onChange={updateImage} />
-                            </div>
-                            <div className="form-group my-5 text-center">
-                                <p className='text-danger justify-content-center' > {formErrors} </p>
-                                <Link to="/blog" type="submit" className="btn btn-dark" onClick={() => handleCreate()} >
-                                    Create post
-                                </Link>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <Footer />
+  return (
+    <div>
+      <Header />
+      <div className="container page-title">
+        <p className="text-center py-4">BLOG</p>
+      </div>
+      <div className="container bg-secondary">
+        <div className="row justify-content-center">
+          <div className="col-md-8 col-md-offset-2">
+            <h1 className="text-center my-4">Create a new post</h1>
+            <form>
+              <div className="form-group mb-4">
+                <label htmlFor="title">Title</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="title"
+                  onChange={updateTitle}
+                />
+              </div>
+              <div className="form-group mb-4">
+                <label htmlFor="description">Description</label>
+                <textarea
+                  rows={5}
+                  className="form-control"
+                  name="description"
+                  defaultValue={""}
+                  onChange={updateContent}
+                />
+              </div>
+              <div className="form-group mb-4">
+                <label htmlFor="formFile" className="form-label">
+                  Upload image
+                </label>
+                <input
+                  className="form-control"
+                  type="file"
+                  id="formFile"
+                  onChange={updateImage}
+                />
+              </div>
+              <div className="form-group my-5 text-center">
+                <p className="text-danger justify-content-center">
+                  {" "}
+                  {formErrors}{" "}
+                </p>
+                <Link
+                  to="/blog"
+                  type="submit"
+                  className="btn btn-dark"
+                  onClick={() => handleCreate()}
+                >
+                  Create post
+                </Link>
+              </div>
+            </form>
+          </div>
         </div>
-    )
+      </div>
+      <Footer />
+    </div>
+  );
 }
--- front-end/src/components/post/post.js
+++ front-end/src/components/post/post.js
@@ -1,121 +1,139 @@
 import React from "react";
 import Footer from "../footer/footer";
 import Header from "../header/header";
-import Comment from "./comment"
+import Comment from "./comment";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { handleCommentAPI } from "../../services/customerService";
+import { handleCommentAPI } from "../../services/postService";
 require("dotenv").config();
 
 export default function Post(props) {
-	const [post, setPost] = useState([]);
-	const [comment, setComment] = useState([]);
-	const [customer, setCustomer] = useState([]);
-	const [comment_input, setCommentInput] = useState("");
-	const [formErrors, setFormErrors] = useState("");
+  const [post, setPost] = useState([]);
+  const [comment, setComment] = useState([]);
+  const [customer, setCustomer] = useState([]);
+  const [comment_input, setCommentInput] = useState("");
+  const [formErrors, setFormErrors] = useState("");
 
-	const customer_id = JSON.parse(localStorage.getItem("user-info"))?.id
+  const customer_id = JSON.parse(localStorage.getItem("user-info"))?.id;
 
-	useEffect(async () => {
-		const result = await axios(
-			process.env.REACT_APP_SERVER_URL + "/posts/" + props.match.params.id
-		);
-		setPost(result.data);
-		setComment(result.data?.comments)
+  useEffect(async () => {
+    const result = await axios(
+      process.env.REACT_APP_SERVER_URL + "/posts/" + props.match.params.id
+    );
+    setPost(result.data);
+    setComment(result.data?.comments);
 
-		const result2 = await axios(
-			process.env.REACT_APP_SERVER_URL + "/customers/" + customer_id
-		);
-		setCustomer(result2.data)
-	});
-	
-	const handleComment = async () => {
-		console.log(comment_input)
-		if (comment_input === "") {
-			setFormErrors("Comment cannot be empty")
-			console.log(formErrors)
-		}
-		else {
-			try {
-				
-				let dataComment = {
-					content: comment_input,
-					customer_id: customer_id,
-					post_id: props.match.params.id
-				}
+    const result2 = await axios(
+      process.env.REACT_APP_SERVER_URL + "/customers/" + customer_id
+    );
+    setCustomer(result2.data);
+  });
 
-				// console.log(dataComent)
-				await handleCommentAPI(dataComment).then((response) => {
-					console.log(response);
-				})
+  const handleComment = async () => {
+    console.log(comment_input);
+    if (comment_input === "") {
+      setFormErrors("Comment cannot be empty");
+      console.log(formErrors);
+    } else {
+      try {
+        let dataComment = {
+          content: comment_input,
+          customer_id: customer_id,
+          post_id: props.match.params.id,
+        };
 
-				setFormErrors(null)
-				setCommentInput("")
+        // console.log(dataComent)
+        await handleCommentAPI(dataComment).then((response) => {
+          console.log(response);
+        });
 
-			} catch (error) {
-				console.log(error)
-			}
-		}
-	}
+        setFormErrors(null);
+        setCommentInput("");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
-	return (
-		<div>
-			<Header />
-			<div className="container page-title">
-				<p className="text-center py-4">BLOG</p>
-			</div>
+  return (
+    <div>
+      <Header />
+      <div className="container page-title">
+        <p className="text-center py-4">BLOG</p>
+      </div>
 
-			<div className="container text-center">
-				<div className="post-title ">
-					<h1> {post?.title} </h1>
-					<p>
-						Post on {post?.published_at?.substring(0, 10)}_{post?.customer?.username}
-					</p>
-				</div>
-				<p className="post-content mx-auto">{post?.content}</p>
-				<img
-					className="mb-5"
-					src={process.env.REACT_APP_SERVER_URL + post?.avatar?.url}
-					alt=""
-					style={{ width: "50%" }}
-				/>
-			</div>
-			<div class="d-flex justify-content-center row">
-				<div class="col-md-8">
-					<div class="d-flex flex-column comment-section">
-						{
-							post.lockComment === false ? (comment.map((value) => {
-								return (
-									<Comment
-										id={value?.customer}
-										content={value?.content}
-										createdAt={value?.createdAt?.substring(0, 10)}
-									/>
-								)
-							})
-							) : null
-						}
-						{
-							customer_id ?  (
-								<div class="bg-light p-2">
-									<div class="d-flex flex-row align-items-start">
-										<img class="rounded-circle" src={process.env.REACT_APP_SERVER_URL + customer?.avatar?.url} width="40" />
-										<textarea class="form-control ml-1 shadow-none textarea" type="input" value={comment_input} onChange={(e) => setCommentInput(e.target.value)} />
-									</div>
-									<div class="mt-2 text-right">
-										<p style={{color:"red"}}> {formErrors} </p>
-										<button class="btn btn-primary btn-sm shadow-none" type="button" onClick={() => handleComment()}>Post comment</button>
-										<button class="btn btn-outline-primary btn-sm ml-1 shadow-none"  type="button" onClick={() => {setCommentInput("")}} >Cancel</button>
-				
-									</div>
-								</div>
-							) : null
-						}
-
-					</div>
-				</div>
-			</div>
-			<Footer />
-		</div>
-	);
+      <div className="container text-center">
+        <div className="post-title ">
+          <h1> {post?.title} </h1>
+          <p>
+            Post on {post?.published_at?.substring(0, 10)}_
+            {post?.customer?.username}
+          </p>
+        </div>
+        <p className="post-content mx-auto">{post?.content}</p>
+        <img
+          className="mb-5"
+          src={process.env.REACT_APP_SERVER_URL + post?.avatar?.url}
+          alt=""
+          style={{ width: "50%" }}
+        />
+      </div>
+      <div class="d-flex justify-content-center row">
+        <div class="col-md-8">
+          <div class="d-flex flex-column comment-section">
+            {post.lockComment === false
+              ? comment.map((value) => {
+                  return (
+                    <Comment
+                      id={value?.customer}
+                      content={value?.content}
+                      createdAt={value?.createdAt?.substring(0, 10)}
+                    />
+                  );
+                })
+              : null}
+            {customer_id ? (
+              <div class="bg-light p-2">
+                <div class="d-flex flex-row align-items-start">
+                  <img
+                    class="rounded-circle"
+                    src={
+                      process.env.REACT_APP_SERVER_URL + customer?.avatar?.url
+                    }
+                    width="40"
+                  />
+                  <textarea
+                    class="form-control ml-1 shadow-none textarea"
+                    type="input"
+                    value={comment_input}
+                    onChange={(e) => setCommentInput(e.target.value)}
+                  />
+                </div>
+                <div class="mt-2 text-right">
+                  <p style={{ color: "red" }}> {formErrors} </p>
+                  <button
+                    class="btn btn-primary btn-sm shadow-none"
+                    type="button"
+                    onClick={() => handleComment()}
+                  >
+                    Post comment
+                  </button>
+                  <button
+                    class="btn btn-outline-primary btn-sm ml-1 shadow-none"
+                    type="button"
+                    onClick={() => {
+                      setCommentInput("");
+                    }}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
