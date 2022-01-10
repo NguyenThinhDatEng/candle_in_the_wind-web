33676155daa703f043fb1f3e235650bfc2540978
dinhphu2k1-gif <dinhphu012001@gmail.com>
2022-01-10 23:48:18 +0700

    add delete post

--- front-end/src/components/post/post.js
+++ front-end/src/components/post/post.js
@@ -1,31 +1,62 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Footer from "../footer/footer";
 import Header from "../header/header";
-
 import Comments from "../comments/Comments";
-import axios from "axios";
 import { useEffect, useState } from "react";
-import { createCommentAPI } from "../../services/commentService"
-import { getUserInfo } from "../../services/customerService"
+import ReactLoading from "react-loading";
+import Modal from 'react-modal';
+import {
+	lockCommentAPI,
+	deletePostAPI
+} from "../../services/postService";
+import axios from "axios";
 require("dotenv").config();
 
+const customStyles = {
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)',
+	},
+};
+
 export default function Post(props) {
-  const [post, setPost] = useState([]);
-  const [comment, setComment] = useState([]);
-  const [customer, setCustomer] = useState([]);
-  const [comment_input, setCommentInput] = useState("");
-  const [formErrors, setFormErrors] = useState("");
+	const [post, setPost] = useState([]);
+	const [comment, setComment] = useState([]);
+	const [lock, setLock] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [confirm, setConfirm] = useState(false);
 
-  const customer_id = JSON.parse(localStorage.getItem("user-info"))?.id;
 
-  useEffect(async () => {
-    const result = await axios(
-      process.env.REACT_APP_SERVER_URL + "/posts/" + props.match.params.id
-    );
-    setPost(result.data);
-    setComment(result.data?.comments);
-		setCustomer(await getUserInfo(customer_id))
+	const customer_id = JSON.parse(localStorage.getItem("user-info"))?.id;
+
+	useEffect(async () => {
+		const result = await axios(
+			process.env.REACT_APP_SERVER_URL + "/posts/" + props.match.params.id
+		);
+		setPost(result.data);
+		setComment(result.data?.comments);
+		setLock(post?.lockComment)
+		setLoading(false)
 	});
+
+	const lockComment = async () => {
+		setLoading(true)
+		const check = lock;
+		await lockCommentAPI(!check, props.match.params.id).then((response) => {
+			console.log(response)
+		})
+	}
+
+	const deletePost = async () => {
+		await deletePostAPI(props.match.params.id).then((response) => {
+			console.log(response)
+		})
+	}
 
 	return (
 		<div>
@@ -34,66 +65,100 @@ export default function Post(props) {
 				<p className="text-center py-4">BLOG</p>
 			</div>
 
-			<div className="container text-center">
-				<div className="post-title ">
-					<h1> {post?.title} </h1>
-					<p>
-						Post on {new Date(post?.published_at).toLocaleDateString()}_{post?.customer?.username}
-					</p>
-				</div>
-				<p className="post-content mx-auto">{post?.content}</p>
-				<img
-					className="mb-5"
-					src={process.env.REACT_APP_SERVER_URL + post?.avatar?.url}
-					alt=""
-					style={{ width: "50%" }}
-				/>
-			</div>
-			<div class="d-flex justify-content-center row">
-				<div class="col-md-8">
-					<div class="d-flex flex-column comment-section">
-						{/* {
-							post.lockComment === false ? (comment.map((value) => {
-								return (
-									<Comment
-										comment_id = {value?._id}
-										customer_id={value?.customer}
-										content={value?.content}
-										createdAt={value?.createdAt?.substring(0, 10)}
-										handleDelete={() => handleDelete(value?._id)}
-									/>
-								)
-							})
-							) : null
-						}
-						{
-							customer_id ?  (
-								<div class="bg-light p-2">
-									<div class="d-flex flex-row align-items-start">
-										<img class="rounded-circle" src={process.env.REACT_APP_SERVER_URL + customer?.avatar?.url} width="40" />
-										<textarea class="form-control ml-1 shadow-none textarea" type="input" value={comment_input} onChange={(e) => setCommentInput(e.target.value)} />
-									</div>
-									<div class="mt-2 text-right">
-										<p style={{color:"red"}}> {formErrors} </p>
-										<button class="btn btn-primary btn-sm shadow-none" type="button" onClick={() => handleComment()}>Post comment</button>
-										<button class="btn btn-outline-primary btn-sm ml-1 shadow-none"  type="button" onClick={() => {setCommentInput("")}} >Cancel</button>
-				
-									</div>
-								</div>
-							) : null
-						} */}
-						{
-							post.lockComment === false ?
-								<Comments
-									commentData={comment}
-									currentUserId={customer_id}
-									postId = {post?._id}
-								/>
-								: null
-						}
+			{
+				loading ?
+					<>
+						<div className="clearfix" />
+						<div className="d-flex justify-content-center">
+							<ReactLoading
+								type="spinningBubbles"
+								color="black"
+								height={200}
+								width={100}
+							/>
+						</div>
+					</>
+					:
+				<>
+					<div className="container text-center">
+						<div className="post-title ">
+							<h1> {post?.title}
+								{
+									customer_id === post?.customer?.id ?
+										<>
+											<div className="btn-group float-end">
+												<button type="button" className="btn dropdown-toggle btn-primary" data-bs-toggle="dropdown"></button>
+												<div class="dropdown-menu " >
+													<div className="form-check form-switch dropdown-item " >
+														<label className="form-check-label ">Lock Comment</label>
+														<input className="form-check-input " type="checkbox" checked={lock} onClick={() => lockComment()} />
+													</div>
+													<button type="button" className="dropdown-item" onClick={() => setConfirm(true)}> Delete </button>
+												</div>
+											</div>
+										</>
+										: null
+								}
+							</h1>
+							<p>
+								Post on {new Date(post?.published_at).toLocaleDateString()}_{post?.customer?.username}
+							</p>
+						</div>
+
+						<p className="post-content mx-auto">{post?.content}</p>
+						<img
+							className="mb-5"
+							src={process.env.REACT_APP_SERVER_URL + post?.avatar?.url}
+							alt=""
+							style={{ width: "50%" }}
+						/>
 					</div>
+					<div class="d-flex justify-content-center row">
+						<div class="col-md-8">
+							<div class="d-flex flex-column comment-section">
+								{
+									lock === false ?
+										<Comments
+											commentData={comment}
+											currentUserId={customer_id}
+											postId={post?._id}
+										/>
+										: null
+								}
+							</div>
+						</div>
+					</div>
+				</>
+			}
+
+			<Modal
+				isOpen={confirm}
+				style={customStyles}
+			>
+
+				<p>Are you sure you want to remove post?</p>
+				<div className="text-center">
+					<Link
+						type="button"
+						className="btn btn-primary btn-sm shadow-none"
+						onClick={() => deletePost()}
+						to="/blog"
+					>
+						Yes
+					</Link>
+					<button
+						type="button"
+						className="btn btn-danger btn-sm ml-1 shadow-none"
+						onClick={() => setConfirm(false)}
+					>
+						No
+					</button>
 				</div>
-			</div>
+
+
+			</Modal>
+
+
 			<Footer />
 		</div>
 	);
