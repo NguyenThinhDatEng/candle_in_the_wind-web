bf8857aee749b6d5043581c06c393d9ee89e69dc
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2022-01-13 21:24:35 +0700

    updated findOne api of post collection

--- back-end/api/post/controllers/post.js
+++ back-end/api/post/controllers/post.js
@@ -40,6 +40,7 @@ const findOne = async (ctx) => {
     title: post.title,
     content: post.content,
     published_at: post.published_at,
+    username: post.customer.username,
     avatar: post.avatar.url,
     comments: allOfComments,
   };
