cffe2fd6147b5a4bea19f73189c44814aad03d7a
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2022-01-13 19:53:46 +0700

    update findOne api in cart

--- back-end/api/cart/services/cart.js
+++ back-end/api/cart/services/cart.js
@@ -1,7 +1,17 @@
 "use strict";
+const Response = require(`../../../utils/response`);
 
 const create = async (customer) => {
   return await strapi.query("cart").create({ customer });
 };
 
-module.exports = { create };
+const err500 = (ctx, error, title) => {
+  console.log(`\n${title}\n`, error);
+  return Response.internalServerError(ctx, {
+    data: null,
+    msg: `Server Error at ${title}`,
+    status: 500,
+  });
+};
+
+module.exports = { create, err500 };
