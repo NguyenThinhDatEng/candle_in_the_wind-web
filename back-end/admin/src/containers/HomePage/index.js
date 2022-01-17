4c647469891f4aa9b9fe3589bb36068db0382d5b
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2022-01-18 02:45:32 +0700

    updated homepage

--- back-end/admin/src/containers/HomePage/index.js
+++ back-end/admin/src/containers/HomePage/index.js
@@ -1,19 +1,22 @@
 import React, { memo } from "react";
 import { auth } from "strapi-helper-plugin";
 import { Block, Container } from "./components";
+import LOGO from "../../assets/images/logo.png";
 
 const HomePage = ({ global: { plugins }, history: { push } }) => {
   const name = auth?.getUserInfo();
 
   return (
     <>
-      <Container className="container-fluid">
-        <div className="row">
-          <div className="col-12">
-            <Block>Hi {name?.firstname}!</Block>
-          </div>
-        </div>
-      </Container>
+      {/* <Container className="container-fluid"> */}
+      <div className="image col-12">
+        <img
+          // style={{ objectFit: "contain" }}
+          src={LOGO}
+          alt=""
+        />
+      </div>
+      {/* </Container> */}
     </>
   );
 };
