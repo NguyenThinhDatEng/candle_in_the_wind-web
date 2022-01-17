6cf65e3747bd884e1ed0bb1e49569c5a1b649895
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2022-01-17 08:08:46 +0700

    ...

--- front-end/src/components/post/create.post.js
+++ front-end/src/components/post/create.post.js
@@ -10,123 +10,127 @@ import ReactHtmlParser from "react-html-parser";
 import axios from "axios";
 require("dotenv").config();
 
-
 export default function CreatePost() {
-	const [state, setState] = useState({
-		title: "",
-		content: "",
-		overview: ""
-	});
-	const [formErrors, setFormErrors] = useState("");
+  const [state, setState] = useState({
+    title: "",
+    content: "",
+    overview: "",
+  });
+  const [formErrors, setFormErrors] = useState("");
 
-	const customer_id = JSON.parse(localStorage.getItem("user-info"))?.id;
+  const customer_id = JSON.parse(localStorage.getItem("user-info"))?.id;
 
-	const updateTitle = (e) => {
-		setState((previousState) => {
-			return { ...previousState, title: e.target.value };
-		});
-	};
+  const updateTitle = (e) => {
+    setState((previousState) => {
+      return { ...previousState, title: e.target.value };
+    });
+  };
 
-	const updateContent = (e, editor) => {
-		// console.log(editor)
-		// console.log(e.target.files[0])
-		setState((previousState) => {
-			return { ...previousState, content: editor.getData().replace("/uploads", "http://localhost:2021/uploads") };
-		});
+  const updateContent = (e, editor) => {
+    // console.log(editor)
+    // console.log(e.target.files[0])
+    setState((previousState) => {
+      return {
+        ...previousState,
+        content: editor
+          .getData()
+          .replaceAll("/uploads", "http://localhost:2021/uploads"),
+      };
+    });
 
-		console.log(state.content);
-	};
+    console.log(state.content);
+  };
 
-	const updateOveriew = (e) => {
-		setState((previousState) => {
-			return { ...previousState, overview: e.target.value };
-		});
-	};
+  const updateOveriew = (e) => {
+    setState((previousState) => {
+      return { ...previousState, overview: e.target.value };
+    });
+  };
 
-	const createPost = async () => {
-		// console.log(state.title, state.content, state.image)
-		if (!state.title) {
-			setFormErrors("Title cannot be empty ");
-			console.log(formErrors);
-		} else if (!state.content) {
-			setFormErrors("Content cannot be empty");
-			console.log(formErrors);
-		} else {
-			setFormErrors("");
-			let data = {
-				title: state.title,
-				content: state.content,
-				overview: state.overview,
-				customer_id: customer_id,
-			};
+  const createPost = async () => {
+    // console.log(state.title, state.content, state.image)
+    if (!state.title) {
+      setFormErrors("Title cannot be empty ");
+      console.log(formErrors);
+    } else if (!state.content) {
+      setFormErrors("Content cannot be empty");
+      console.log(formErrors);
+    } else {
+      setFormErrors("");
+      let data = {
+        title: state.title,
+        content: state.content,
+        overview: state.overview,
+        customer_id: customer_id,
+      };
 
-			console.log(data);
-			await createPostAPI(data).then((response) => {
-				console.log(response);
-			});
-		}
-	};
+      console.log(data);
+      await createPostAPI(data).then((response) => {
+        console.log(response);
+      });
+    }
+  };
 
-	return (
-		<div>
-			<Header />
-			<div className="container">
-				<div className="row justify-content-center">
-					<div className="col-md-8 col-md-offset-2">
-						<h1 className="text-center my-4">Create a new post</h1>
-						<form>
-							<div className="form-group mb-4">
-								<label htmlFor="title">Title</label>
-								<input
-									type="text"
-									className="form-control"
-									name="title"
-									onChange={updateTitle}
-								/>
-							</div>
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-8 col-md-offset-2">
+            <h1 className="text-center my-4">Create a new post</h1>
+            <form>
+              <div className="form-group mb-4">
+                <label htmlFor="title">Title</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="title"
+                  onChange={updateTitle}
+                />
+              </div>
 
-							<div>
-								<article>Content</article>
-								<CKEditor
-									editor={ClassicEditor}
-									config={{
-										simpleUpload: {
-											uploadUrl: process.env.REACT_APP_SERVER_URL + "/upload",
-										},
-									}}
-									onChange={updateContent}
-								/>
-						
-								{/* {ReactHtmlParser(state.content)} */}
-							</div>
-							<div className="form-group mb-4">
-								<label htmlFor="description">Overview</label>
-								<textarea
-									rows={5}
-									className="form-control"
-									defaultValue={""}
-									onChange={updateOveriew}
-								/>
-							</div>
-							<div className="form-group my-5 text-center">
-								<p className="text-danger justify-content-center">
-									{" "}
-									{formErrors}{" "}
-								</p>
-								<Link
-									to="/blog"
-									type="submit"
-									className="btn btn-dark"
-									onClick={() => createPost()}
-								>
-									Create post
-								</Link>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-			<Footer />
-		</div>
-	);
+              <div>
+                <article>Content</article>
+                <CKEditor
+                  editor={ClassicEditor}
+                  config={{
+                    simpleUpload: {
+                      uploadUrl: process.env.REACT_APP_SERVER_URL + "/upload",
+                    },
+                  }}
+                  onChange={updateContent}
+                />
+
+                {/* {ReactHtmlParser(state.content)} */}
+              </div>
+              <div className="form-group mb-4">
+                <label htmlFor="description">Overview</label>
+                <textarea
+                  rows={5}
+                  className="form-control"
+                  defaultValue={""}
+                  onChange={updateOveriew}
+                />
+              </div>
+              <div className="form-group my-5 text-center">
+                <p className="text-danger justify-content-center">
+                  {" "}
+                  {formErrors}{" "}
+                </p>
+                <Link
+                  to="/blog"
+                  type="submit"
+                  className="btn btn-dark"
+                  onClick={() => createPost()}
+                >
+                  Create post
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
